92f4809392aac012d11ef90128a9668da5ca497e
Salman Dabbakuti <dabbakutisalman@gmail.com>
2019-03-25 00:15:23 +0530

    Update pythonAz.py

--- pythonAz.py
+++ pythonAz.py
@@ -24,16 +24,16 @@ print(name.upper())
 z = input("Enter Your Name: ")
 print("Hello," + z)
 if x==5:
-    print "X is five"
-    else:
-        print "X is not five"
-        if x!=4:
-        print("X is not Four")
-        print("X is greater than 3 and less than 6:", x>3 and x<6)
-        fruits=["Apple","Banana","Mango","Strawberry"]
-        fruits[1]="Grapes"
-        print(fruits[1])
-        for x in fruits:
-            print(x)
-            if x == "Mango":
-                break
+    print ("X is five")
+else:
+    print ("X is not five")
+if x!=4:
+    print("X is not Four")
+    print("X is greater than 3 and less than 6:", x>3 and x<6)
+fruits=["Apple","Banana","Mango","Strawberry"]
+fruits[1]="Grapes"
+print(fruits[1])
+for x in fruits:
+    print(x)
+    if x == "Mango":
+        break
